403f80db71c804bba1fe8eb11a69886b93dd856a
Sayan Saha <sayansaha1082@gmail.com>
2020-12-24 23:43:45 -0800

    Add simple page layout

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.set('view engine', 'ejs');
+
+app.use('/public', express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.render('index');
 })
 
 app.listen(port, () => {
